bb85cf24c1728c1d121ccd0bfb58ef909ee175f0
Fellipe Guimarães <135441307+fsguimaraes@users.noreply.github.com>
2025-05-22 20:47:21 -0300

    Add files via upload

--- carrossel_portfolio.js
+++ carrossel_portfolio.js
@@ -1,26 +1,24 @@
+const track = document.querySelector('.carrossel-track');
+const btnLeft = document.querySelector('.nav-button.left');
+const btnRight = document.querySelector('.nav-button.right');
 
-  // EFEITO GALERIA (CARROSSEL)
-  const track = document.querySelector('.carrossel-track');
-  const btnLeft = document.querySelector('.nav-button.left');
-  const btnRight = document.querySelector('.nav-button.right');
+let currentIndex = 0;
 
-  let currentIndex = 0;
-  const itemWidth = 300 + 30; // largura + gap
-  const itemsVisible = 4;
-  const totalItems = document.querySelectorAll('.img-port').length;
+const itemWidth = 300 + 20; // largura + gap
+const itemsVisible = 4;
+const totalItems = document.querySelectorAll('.img-port').length;
 
-  btnRight.addEventListener('click', () => {
-    if (currentIndex < totalItems - itemsVisible) {
-      currentIndex++;
-      track.style.transform = `translateX(-${currentIndex * 10}%)`;
-    }
-  });
+btnRight.addEventListener('click', () => {
+  if (currentIndex < totalItems - itemsVisible) {
+    currentIndex++;
+    track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+  }
+});
 
-  btnLeft.addEventListener('click', () => {
-    if (currentIndex > 0) {
-      currentIndex--;
-      track.style.transform = `translateX(-${currentIndex * 10}%)`;
-    }
-  });
-
+btnLeft.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+  }
+});
 
